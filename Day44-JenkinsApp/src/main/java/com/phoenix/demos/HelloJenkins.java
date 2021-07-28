dac34ea565de36090d5287afc926319d76bf17ba
Phoenix-FireBird <phoenix.firebird@gmail.com>
2021-07-29 00:39:52 +0530

    Committing

---
+++ Day44-JenkinsApp/src/main/java/com/phoenix/demos/HelloJenkins.java
@@ -0,0 +1,10 @@
+package com.phoenix.demos;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+	}
+
+}
