d0187152483dd50b6cbf692a0673c0872dc8180d
Phoenix-FireBird <phoenix.firebird@gmail.com>
2021-07-29 14:58:00 +0530

    Removed the to do

--- Day44-JenkinsApp/src/main/java/com/phoenix/demos/HelloJenkins.java
+++ Day44-JenkinsApp/src/main/java/com/phoenix/demos/HelloJenkins.java
@@ -3,7 +3,6 @@ package com.phoenix.demos;
 public class HelloJenkins {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello Jenkins");
 		System.out.println("something else");
 	}
